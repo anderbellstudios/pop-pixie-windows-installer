f5c46d0b17ab9d8ec9da2893b5ca0cb15ebf13e3
Joe Anderson <joe@mousetrapped.co.uk>
2022-11-12 11:48:10 +0000

    Fix: Incorrectly reports success when Docker isn't running

--- binding.rb
+++ binding.rb
@@ -1,11 +1,14 @@
 module WindowsPackager
   def self.package(build)
     destination_dir = File.join(__dir__, 'Pop Pixie')
+
     FileUtils.rm_rf(destination_dir)
     build.mv(destination_dir)
-    system File.join(__dir__, 'build-installer')
-    output_path = File.join(__dir__, 'Output', 'Install Pop Pixie.exe')
-    raise 'Failed to create Windows installer' unless File.exist?(output_path)
-    return output_path
+
+    unless system(File.join(__dir__, 'build-installer'))
+      raise 'Failed to create Windows installer'
+    end
+
+    return File.join(__dir__, 'Output', 'Install Pop Pixie.exe')
   end
 end
